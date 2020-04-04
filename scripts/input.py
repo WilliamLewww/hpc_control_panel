8ce72d87ddff61f1c519fad3112d352390439eb7
William Lew <WilliamMilesLew@gmail.com>
2020-04-03 20:31:29 -0700

    added: files

---
+++ scripts/input.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+input_pin = 22
+
+def main():
+    prev_value = None
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(input_pin, GPIO.IN)
+    print("Starting demo now! Press CTRL+C to exit")
+    try:
+        while True:
+            value = GPIO.input(input_pin)
+            if value != prev_value:
+                if value == GPIO.HIGH:
+                    value_str = "HIGH"
+                else:
+                    value_str = "LOW"
+                print("Value read from pin {} : {}".format(input_pin,
+                                                           value_str))
+                prev_value = value
+            time.sleep(1)
+    finally:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
+
