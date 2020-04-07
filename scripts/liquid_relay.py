ca64f63e22deab5725d0f2c456119e720fba04d1
William Lew <WilliamMilesLew@gmail.com>
2020-04-07 11:48:46 -0700

    added: liquid script

---
+++ scripts/liquid_relay.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitions
+left_relay = 17  # BOARD pin 3, BCM pin 11
+right_relay = 18 # BOARD pin 5, BCM pin 12
+
+def main():
+    # Pin Setup:
+    # Board pin-numbering scheme
+    GPIO.setmode(GPIO.BCM)
+    # set pin as an output pin with optional initial state of HIGH
+    # GPIO.setup(left_relay, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(right_relay, GPIO.OUT, initial=GPIO.LOW)
+
+if __name__ == '__main__':
+    main()
